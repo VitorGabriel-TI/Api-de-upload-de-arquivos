d528e286f2a060368636d12bbf60460266900d4d
Vitor Gabriel <contato.vitorgabrielti@gmail.com>
2024-01-25 06:19:04 -0300

    Rota de download adicionada

--- src/main/java/br/com/vitorgabrielti/filestorageapi/FileStorageController.java
+++ src/main/java/br/com/vitorgabrielti/filestorageapi/FileStorageController.java
@@ -1,17 +1,26 @@
 package br.com.vitorgabrielti.filestorageapi;
 
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import jakarta.servlet.http.HttpServletRequest;
 
 
 
@@ -42,5 +51,26 @@ public class FileStorageController {
         } catch (IOException ex) {
             return ResponseEntity.badRequest().build();
         }
+    }
+
+    @GetMapping("download/{fileName:.+}")
+    public ResponseEntity<Resource> downloadFile(@PathVariable String fileName, HttpServletRequest request) throws IOException{
+        Path filePath = fileStorageLocation.resolve(fileName).normalize();
+        try {
+            Resource resource = new UrlResource(filePath.toUri());
+            String contentType = request.getServletContext().getMimeType(resource.getFile().getAbsolutePath());
+            
+            if (contentType == null)
+                contentType = "application/octet-stream";
+            
+            return ResponseEntity.ok()
+                .contentType(MediaType.parseMediaType(contentType))
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + resource.getFilename() + "\"")
+                .body(resource);
+
+        } catch (MalformedURLException e) {
+            return ResponseEntity.badRequest().build();
+        }
+        
     }
 }
