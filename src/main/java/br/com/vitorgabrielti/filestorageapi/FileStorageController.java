c1c348bc7574cecde6dd084889d6ebba2ca2c4c6
Vitor Gabriel <contato.vitorgabrielti@gmail.com>
2024-01-25 05:14:39 -0300

    Adição do diretório de upload

--- src/main/java/br/com/vitorgabrielti/filestorageapi/FileStorageController.java
+++ src/main/java/br/com/vitorgabrielti/filestorageapi/FileStorageController.java
@@ -1,10 +1,19 @@
 package br.com.vitorgabrielti.filestorageapi;
 
+import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+
 
 @Controller
 @RequestMapping("/api/files")
@@ -14,5 +23,24 @@ public class FileStorageController {
     public FileStorageController(FileStorageProperties fileStorageProperties){
         this.fileStorageLocation = Paths.get(fileStorageProperties.getUploadDir())
             .toAbsolutePath().normalize();
+    }
+
+    @PostMapping("/upload")
+    public ResponseEntity<String> uploadFile(@RequestParam("file") MultipartFile file){
+        String fileName = StringUtils.cleanPath(file.getOriginalFilename());
+
+        try {
+            Path targetLocation = fileStorageLocation.resolve(fileName);
+            file.transferTo(targetLocation);
+
+            String fileDownloadUri = ServletUriComponentsBuilder.fromCurrentContextPath()
+                .path("/api/files/download")
+                .path(fileName)
+                .toUriString();
+
+            return ResponseEntity.ok("Upload completed! Download link" + fileDownloadUri);
+        } catch (IOException ex) {
+            return ResponseEntity.badRequest().build();
+        }
     }
 }
